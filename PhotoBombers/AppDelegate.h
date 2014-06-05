fa6e7bb9d36f7bb0153cc77dd98550e235bd6c2b
Joao Pescada <joao.pescada@gmail.com>
2014-06-05 12:28:36 +0100

    Initial Commit

---
+++ PhotoBombers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhotoBombers
+//
+//  Created by Joao Pescada on 05/06/2014.
+//  Copyright (c) 2014 Joao Pescada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
